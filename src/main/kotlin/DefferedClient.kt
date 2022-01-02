70e6c22405102527136a5ee0fed464d02768ce4b
Brent Gardner <bgardner@squarelabs.net>
2022-01-01 18:29:04 -0700

    Decent API

--- src/main/kotlin/DefferedClient.kt
+++ src/main/kotlin/DefferedClient.kt
@@ -1,5 +1,7 @@
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.future.await
 import kotlinx.coroutines.launch
 import org.eclipse.paho.mqttv5.client.*
 import org.eclipse.paho.mqttv5.client.persist.MqttDefaultFilePersistence
@@ -7,6 +9,9 @@ import org.eclipse.paho.mqttv5.common.MqttException
 import org.eclipse.paho.mqttv5.common.MqttMessage
 import org.eclipse.paho.mqttv5.common.packet.MqttProperties
 import java.util.*
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.TimeoutException
 
 class DefferedClient(
     url: String,
@@ -14,7 +19,10 @@ class DefferedClient(
 ) : MqttCallback {
     val publisherId = UUID.randomUUID().toString()
     val client = MqttAsyncClient(url, publisherId, MqttDefaultFilePersistence("/tmp"))
+    val fromMqtt = Channel<Pair<String, MqttMessage>>()
     val incoming = Channel<Pair<String, MqttMessage>>()
+    val requests = ConcurrentHashMap<String, CompletableFuture<String>>()
+    val responseTopic = "responses"
 
     init {
         client.setCallback(this)
@@ -26,12 +34,55 @@ class DefferedClient(
         options.isCleanStart = true
         options.connectionTimeout = 10
         client.connect(options).waitForCompletion()
+        subscribe(responseTopic, 0)
+        scope.launch {
+            for(msg in fromMqtt) {
+                if(msg.first == responseTopic) {
+                    val requestId = String(msg.second.properties.correlationData)
+                    val payload = String(msg.second.payload)
+//                    println("Got response to $requestId: $payload")
+                    requests.remove(requestId)?.complete(payload)
+                } else {
+                    incoming.send(msg)
+                }
+            }
+        }
     }
 
     fun publish(topic: String, message: MqttMessage, context: Any?): DeferredAction {
         val listener = DeferredAction()
         client.publish(topic, message, context, listener)
         return listener
+    }
+
+    fun respond(request: MqttMessage, response: String) {
+        val msg = MqttMessage(response.toByteArray())
+        msg.qos = 0;
+        msg.properties = MqttProperties()
+        msg.properties.messageExpiryInterval = 1
+        msg.isRetained = false; // do not queue, stale data is bad
+        msg.properties.correlationData = request.properties.correlationData
+        client.publish(responseTopic, msg)
+    }
+
+    suspend fun request(topic: String, payload: String): String? {
+        val uuid = UUID.randomUUID().toString()
+        val msg = MqttMessage(payload.toByteArray())
+        msg.qos = 0;
+        msg.properties = MqttProperties()
+        msg.properties.messageExpiryInterval = 1
+        msg.isRetained = false; // do not queue, stale data is bad
+        msg.properties.responseTopic = responseTopic
+        msg.properties.correlationData = uuid.toByteArray()
+
+        val cofu = CompletableFuture<String>()
+        requests[uuid] = cofu
+        client.publish(topic, msg)
+        scope.launch {
+            delay(3000)
+            requests.remove(uuid)?.completeExceptionally(TimeoutException("Request ${uuid} did not complete"))
+        }
+        return cofu.await()
     }
 
     fun subscribe(topic: String, qos: Int) {
@@ -48,7 +99,8 @@ class DefferedClient(
 
     override fun messageArrived(topic: String?, message: MqttMessage?) {
         scope.launch {
-            incoming.send(Pair(topic!!, message!!))
+//            println("DefferedClient got message")
+            fromMqtt.send(Pair(topic!!, message!!))
         }
     }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,10 @@
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.future.await
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import org.eclipse.paho.mqttv5.common.MqttMessage
-import org.eclipse.paho.mqttv5.common.packet.MqttProperties
 
 fun main() {
     println("Hello World!")
-    val TOPIC = "temps"
+    val reqTopic = "temps"
 
     runBlocking {
         val publisher = DefferedClient("tcp://127.0.0.1:1883", this)
@@ -18,26 +14,22 @@ fun main() {
         val subscribers = (0..1).map { DefferedClient("tcp://127.0.0.1:1883", this) }
         subscribers.forEach {
             it.connect()
-            it.subscribe(TOPIC, 0)
+            it.subscribe(reqTopic, 0)
         }
         println("Subscribers connected!")
         val listeners = subscribers.map {
             async {
                 for(item in it.incoming) {
                     val text = item.second.payload?.let { String(it) }
-                    println("message received: ${text}")
+                    println("message received: $text")
+                    it.respond(item.second, "Got $text")
                 }
             }
         }
 
         // publish
-        val payload = "op1".toByteArray()
-        val msg = MqttMessage(payload)
-        msg.qos = 0;
-        msg.properties = MqttProperties()
-        msg.properties.messageExpiryInterval = 1
-        msg.isRetained = false; // do not queue, stale data is bad
-        publisher.publish(TOPIC, msg, null).await()
+        val resp = publisher.request(reqTopic, "Hello, world!")
+        println("Got response: $resp")
 
         println("Awaiting...")
         listeners.awaitAll()
